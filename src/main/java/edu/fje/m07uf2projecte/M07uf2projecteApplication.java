c763338a4ec5d831e273e33e16e0088cd649007b
Raúl Heredia <raul.heredia01@outlook.com>
2022-04-03 18:45:17 +0200

    First Commit

---
+++ src/main/java/edu/fje/m07uf2projecte/M07uf2projecteApplication.java
@@ -0,0 +1,13 @@
+package edu.fje.m07uf2projecte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M07uf2projecteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M07uf2projecteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/fje/m07uf2projecte/M07uf2projecteApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fje.m07uf2projecte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M07uf2projecteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
